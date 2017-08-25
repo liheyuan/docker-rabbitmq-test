27c3d6bcef04bf03aa3d8112189e33496b382e7c
coder4 <lihy@fenbi.com>
2017-08-25 19:00:58 +0800

    FIX: Bug

--- create_vm.sh
+++ create_vm.sh
@@ -3,7 +3,7 @@ NODE_NAME="node-rabbitmq"
 VOLUME_DATA='$HOME/docker_data/rabbitmq'
 docker-machine create -d virtualbox $NODE_NAME
 docker-machine ssh $NODE_NAME "mkdir -p $VOLUME_DATA"
-#docker-machine ssh $NODE_NAME "echo -e '{\n\"registry-mirrors\": [\t\"https://docker.mirrors.ustc.edu.cn\"]\n}' > ./daemon.json"
-#docker-machine ssh $NODE_NAME "sudo cp ./daemon.json /etc/docker/"
-#docker-machine restart $NODE_NAME
-#docker-machine regenerate-certs -f $NODE_NAME
+docker-machine ssh $NODE_NAME "echo -e '{\n\"registry-mirrors\": [\t\"https://docker.mirrors.ustc.edu.cn\"]\n}' > ./daemon.json"
+docker-machine ssh $NODE_NAME "sudo cp ./daemon.json /etc/docker/"
+docker-machine restart $NODE_NAME
+docker-machine regenerate-certs -f $NODE_NAME
