b1fe40e82d927988ce636737bf407f56eeef5b96
coder4 <lihy@fenbi.com>
2017-08-29 10:03:15 +0800

    ADD: Default vhost

--- run_rabbitmq_docker.sh
+++ run_rabbitmq_docker.sh
@@ -5,4 +5,4 @@ VOLUME_DATA='/var/lib/boot2docker/docker_data/rabbitmq'
 eval $(docker-machine env $NODE_NAME)
 last_id=$(docker ps -l -q)
 docker rm -f $last_id
-docker run --rm --name $DOCKER_NAME -v "$VOLUME_DATA":/var/lib/rabbitmq -e RABBITMQ_DEFAULT_USER=sbmvt -e RABBITMQ_DEFAULT_PASS=hehehe -p15672:15672 -p5672:5672 -d rabbitmq:3-management-alpine 
+docker run --rm --name $DOCKER_NAME -v "$VOLUME_DATA":/var/lib/rabbitmq -e RABBITMQ_DEFAULT_USER=sbmvt -e RABBITMQ_DEFAULT_PASS=hehehe -e RABBITMQ_DEFAULT_VHOST=sbmvt -p15672:15672 -p5672:5672 -d rabbitmq:3-management-alpine 
